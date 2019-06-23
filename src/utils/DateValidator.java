4942812e3bf1794c583eaacfa65a4e97b79b108b
Przemysław Pradela <51158576+PrzemyslawPradela@users.noreply.github.com>
2019-06-23 11:44:05 +0200

    Update DateValidator.java

--- src/utils/DateValidator.java
+++ src/utils/DateValidator.java
@@ -7,10 +7,10 @@ public class DateValidator {
 	private Pattern pattern;
 	private Matcher matcher;
 
-	private static final String DIGITS_PATTERN = "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$";
+	private static final String DATE_PATTERN = "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$";
 
 	public DateValidator() {
-		pattern = Pattern.compile(DIGITS_PATTERN);
+		pattern = Pattern.compile(DATE_PATTERN);
 	}
 
 	public boolean validate(String digits) {
